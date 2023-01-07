ba761a21cff4fbf796b9b1441f07299eeee4e167
Abdulhadi5692HDI2 <110974220+Abdulhadi5692HDI2@users.noreply.github.com>
2023-01-07 15:48:25 +0000

    Setup IRQS

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -27,15 +27,50 @@ void irq_handler() {
     outb(0x20, 0x20);
 }
 extern void* isr_stub_table[];
- 
+extern void* irq1;
+extern void* irq2;
+extern void* irq3;
+extern void* irq4;
+extern void* irq5;
+extern void* irq6;
+extern void* irq7;
+extern void* irq8;
+extern void* irq9;
+extern void* irq10;
+extern void* irq11;
+extern void* irq12;
+extern void* irq13;
+extern void* irq14;
+extern void* irq15;
+
 void idt_init(void);
 void idt_init() {
     idtr.base = (uintptr_t)&idt[0];
     idtr.limit = (uint16_t)sizeof(idt_entry_t) * 256 - 1;
     PIC_remap(0x20, 0x28);
+
+    // ISRS
     for (uint8_t vector = 0; vector < 32; vector++) {
         idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
     }
+
+    // irqs
+    idt_set_descriptor((int)32, irq0, 0x8E);
+    idt_set_descriptor((int)33, irq1, 0x8E);
+    idt_set_descriptor((int)34, irq2, 0x8E);
+    idt_set_descriptor((int)35, irq3, 0x8E);
+    idt_set_descriptor((int)36, irq4, 0x8E);
+    idt_set_descriptor((int)37, irq5, 0x8E);
+    idt_set_descriptor((int)38, irq6, 0x8E);
+    idt_set_descriptor((int)39, irq7, 0x8E);
+    idt_set_descriptor((int)40, irq8, 0x8E);
+    idt_set_descriptor((int)41, irq9, 0x8E);
+    idt_set_descriptor((int)42, irq10, 0x8E);
+    idt_set_descriptor((int)43, irq11, 0x8E);
+    idt_set_descriptor((int)44, irq12, 0x8E);
+    idt_set_descriptor((int)45, irq13, 0x8E);
+    idt_set_descriptor((int)46, irq14, 0x8E);
+    idt_set_descriptor((int)47, irq15, 0x8E);
  
     __asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
     __asm__ volatile ("sti"); // set the interrupt flag
